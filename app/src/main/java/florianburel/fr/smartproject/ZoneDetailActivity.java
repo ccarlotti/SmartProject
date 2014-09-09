089689376c040b7c75ad11a8d75343b6847dfdc6
Florian BUREL <florian.burel@mac.com>
2014-09-09 15:58:55 +0200

    Affichage des infos de la zone

--- app/src/main/java/florianburel/fr/smartproject/ZoneDetailActivity.java
+++ app/src/main/java/florianburel/fr/smartproject/ZoneDetailActivity.java
@@ -4,33 +4,67 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.LinkedHashMap;
 
 
 public class ZoneDetailActivity extends Activity {
+
+    private Zone zone = new Zone("Cuisine");
+
+    private TextView zoneModeTextView;
+    private TextView zoneSetPointTextView;
+    private ListView heatersListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zone_detail);
+
+        bind();
+
+        setDisplay();
+
     }
 
+    private void setDisplay() {
+        setTitle(zone.getName());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.zone_detail, menu);
-        return true;
-    }
+        // Mode
+        switch (zone.getMode())
+        {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            case ECO:
+                zoneModeTextView.setText(getString(R.string.eco));
+                break;
+            case CONFORT:
+                zoneModeTextView.setText(getString(R.string.confort));
+                break;
+            case STOP:
+                zoneModeTextView.setText(getString(R.string.stopped));
+                break;
+            case HORS_GEL:
+                zoneModeTextView.setText(getString(R.string.freeze));
+                break;
+            case PROG:
+                zoneModeTextView.setText(getString(R.string.prog));
+                break;
         }
-        return super.onOptionsItemSelected(item);
+
+        // setPoint
+        String setpoint = zone.getPoint() + "°C";
+        zoneSetPointTextView.setText(setpoint);
+
     }
+
+    private void bind() {
+        zoneModeTextView = (TextView) findViewById(R.id.zoneModeTextView);
+        zoneSetPointTextView = (TextView) findViewById(R.id.zoneSetPointTextView);
+        heatersListView = (ListView) findViewById(R.id.heatersListView);
+    }
+
+
 }
